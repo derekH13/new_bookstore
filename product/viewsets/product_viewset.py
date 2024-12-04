87c714610f11b40139100db168bd87348f625db3
derekH13 <168042164+derekH13@users.noreply.github.com>
2024-12-04 17:35:39 -0300

    feat: docke-compose, postgres

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,18 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    # adicionando as duas classes de autentificação (esta no setting)
-    # BasicAuthentication uma autentificação simples, e token
-    authentication_classes = [SessionAuthentication,
-                              BasicAuthentication, TokenAuthentication]
-    # faz uma interceptação da resquest/cahmada para verificar se o user esta autorizado ou não (atraves do tokken autenfication)
-    permission_classes = [IsAuthenticated]
+
     serializer_class = ProductSerializer
 
     def get_queryset(self):
